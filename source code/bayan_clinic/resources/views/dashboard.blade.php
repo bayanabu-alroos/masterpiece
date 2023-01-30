6d553fcc98b30f48c67bb9d64d6747aa9cfac0a6
bayanabu-alroos <bayan.adnan.alroos@gmail.com>
2023-01-30 21:30:43 +0300

    edit

--- source code/bayan_clinic/resources/views/dashboard.blade.php
+++ source code/bayan_clinic/resources/views/dashboard.blade.php
@@ -37,13 +37,13 @@
         <!-- ============================================================== -->
         <div class="row">
           <!-- Column -->
-          <div class="col-md-6 col-lg-2 col-xlg-3">
+          <div class="col-md-6 col-lg-4 col-xlg-3">
             <div class="card card-hover">
               <div class="box bg-cyan text-center">
                 <h1 class="font-light text-white">
-                  <i class="mdi mdi-view-dashboard"></i>
-                </h1>
-                <h6 class="text-white">Dashboard</h6>
+                  <i class="me-2 mdi mdi-account-multiple"></i
+                    >{{\DB::table('users')->where('users.level', '=' ,'patient')->count()}}</h1>
+                <h6 class="text-white">Number Patient</h6>
               </div>
             </div>
           </div>
@@ -52,43 +52,48 @@
             <div class="card card-hover">
               <div class="box bg-success text-center">
                 <h1 class="font-light text-white">
-                  <i class="mdi mdi-chart-areaspline"></i>
+                  <i class="me-2 mdi mdi-account-multiple"></i
+                    >
+                  {{\DB::table('users')->where('users.level', '=' ,'nurse')->count()}}
+                </h1>
+                <h6 class="text-white">Number Nurse</h6>
+              </div>
+            </div>
+          </div>
+          <!-- Column -->
+          <div class="col-md-6 col-lg-4 col-xlg-3">
+            <div class="card card-hover">
+              <div class="box bg-warning text-center">
+                <h1 class="font-light text-white">
+                  <i class="me-2 mdi mdi-account-multiple"></i
+                    >
                   {{\DB::table('users')->where('users.level', '=' ,'doctor')->count()}}
                 </h1>
-                <h6 class="text-white">Charts</h6>
+                <h6 class="text-white">Number Doctor</h6>
               </div>
             </div>
           </div>
           <!-- Column -->
-          <div class="col-md-6 col-lg-2 col-xlg-3">
-            <div class="card card-hover">
-              <div class="box bg-warning text-center">
-                <h1 class="font-light text-white">
-                  <i class="mdi mdi-collage"></i>
-                </h1>
-                <h6 class="text-white">Widgets</h6>
-              </div>
-            </div>
-          </div>
-          <!-- Column -->
-          <div class="col-md-6 col-lg-2 col-xlg-3">
+          <div class="col-md-6 col-lg-4 col-xlg-3">
             <div class="card card-hover">
               <div class="box bg-danger text-center">
                 <h1 class="font-light text-white">
-                  <i class="mdi mdi-border-outside"></i>
+                  <i class="fas fa-couch"></i>
+                  {{\DB::table('rooms')->count()}}
                 </h1>
-                <h6 class="text-white">Tables</h6>
+                <h6 class="text-white">Number Rooms</h6>
               </div>
             </div>
           </div>
           <!-- Column -->
-          <div class="col-md-6 col-lg-2 col-xlg-3">
+          <div class="col-md-6 col-lg-4 col-xlg-3">
             <div class="card card-hover">
               <div class="box bg-info text-center">
                 <h1 class="font-light text-white">
-                  <i class="mdi mdi-arrow-all"></i>
+                  <i class="fab fa-servicestack"></i
+                    >{{\DB::table('services')->count()}}
                 </h1>
-                <h6 class="text-white">Full Width</h6>
+                <h6 class="text-white">Number Services</h6>
               </div>
             </div>
           </div>
@@ -98,57 +103,16 @@
             <div class="card card-hover">
               <div class="box bg-danger text-center">
                 <h1 class="font-light text-white">
-                  <i class="mdi mdi-receipt"></i>
+                  <i class="me-2 mdi mdi-bio"></i
+                    >{{\DB::table('sessions')->count()}}
                 </h1>
-                <h6 class="text-white">Forms</h6>
+                <h6 class="text-white">Number Sessions</h6>
               </div>
             </div>
           </div>
-          <!-- Column -->
-          <div class="col-md-6 col-lg-2 col-xlg-3">
-            <div class="card card-hover">
-              <div class="box bg-info text-center">
-                <h1 class="font-light text-white">
-                  <i class="mdi mdi-relative-scale"></i>
-                </h1>
-                <h6 class="text-white">Buttons</h6>
-              </div>
-            </div>
-          </div>
-          <!-- Column -->
-          <div class="col-md-6 col-lg-2 col-xlg-3">
-            <div class="card card-hover">
-              <div class="box bg-cyan text-center">
-                <h1 class="font-light text-white">
-                  <i class="mdi mdi-pencil"></i>
-                </h1>
-                <h6 class="text-white">Elements</h6>
-              </div>
-            </div>
-          </div>
-          <!-- Column -->
-          <div class="col-md-6 col-lg-2 col-xlg-3">
-            <div class="card card-hover">
-              <div class="box bg-success text-center">
-                <h1 class="font-light text-white">
-                  <i class="mdi mdi-calendar-check"></i>
-                </h1>
-                <h6 class="text-white">Calnedar</h6>
-              </div>
-            </div>
-          </div>
-          <!-- Column -->
-          <div class="col-md-6 col-lg-2 col-xlg-3">
-            <div class="card card-hover">
-              <div class="box bg-warning text-center">
-                <h1 class="font-light text-white">
-                  <i class="mdi mdi-alert"></i>
-                </h1>
-                <h6 class="text-white">Errors</h6>
-              </div>
-            </div>
-          </div>
-          <!-- Column -->
+
+
+          
         </div>
     
     @if(\Illuminate\Support\Facades\Auth::user()->level == 'admin'  || 'reception'||'doctor')
